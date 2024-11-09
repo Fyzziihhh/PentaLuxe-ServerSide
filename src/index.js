1f82aa5f89151e13bc5a414e1de636f98bb83223
Fyzziihhh 76 <5zziiihhh@gamil.com>
2024-11-10 02:00:53 +0530

    uncomment the mongoStore inthe session

--- src/index.js
+++ src/index.js
@@ -2,55 +2,55 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import userRouter from "./routes/user/user.routes.js";
-import adminRouter from './routes/admin/admin.routes.js'
-import cookieParser from 'cookie-parser'
-import session from 'express-session'
-import nocache from 'nocache'
+import adminRouter from "./routes/admin/admin.routes.js";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import nocache from "nocache";
 dotenv.config({ path: "src/.env" });
-// import MongoStore from 'connect-mongo'; 
+import MongoStore from 'connect-mongo';
 process.setMaxListeners(20);
 const app = express();
-console.log(`MongoDB URL: ${process.env.MONGO_URL}`);
-import cors from 'cors';
+import cors from "cors";
 
-
-app.use(cors({
-  origin: 'https://pentaluxeshop.vercel.app', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'], 
-  allowedHeaders: ['Content-Type', 'Authorization'], 
-  credentials: true, 
-}));
-app.use(session({
-  secret: process.env.ADMIN_SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  // store: MongoStore.create({
-  //   mongoUrl: process.env.MONGO_URL
-  // }),
-  cookie: {
-    maxAge: 24 * 60 * 60 * 1000,
-    httpOnly: true,
-  }
-  
-}));
+app.use(
+  cors({
+    origin: "https://pentaluxeshop.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+app.use(
+  session({
+    secret: process.env.ADMIN_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URL
+    }),
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    },
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(nocache())
+app.use(nocache());
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  console.error('something went wrong')
+  console.error("something went wrong");
   console.error(err.stack);
 
-  res.status(500).json({ message: 'Something went wrong!' });
+  res.status(500).json({ message: "Something went wrong!" });
 });
 app.use(express.static("public"));
 
 // Routers
-app.use('/api/user', userRouter);
-app.use('/api/admin', adminRouter);
-
+app.use("/api/user", userRouter);
+app.use("/api/admin", adminRouter);
 
 const PORT = process.env.PORT || 3000;
 connectDB();
