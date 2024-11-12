578a9145a1e1d38fd3a3e44ea32639a34e815255
Fyzziihhh 76 <5zziiihhh@gamil.com>
2024-11-12 06:55:42 +0530

    Filter sales report to display only orders with 'Shipped', 'Delivered', or 'Confirmed' status in the admin panel

--- src/controllers/admin/admin.sales.controller.js
+++ src/controllers/admin/admin.sales.controller.js
@@ -1,39 +1,48 @@
-import { createResponse, serverErrorResponse } from "../../helpers/responseHandler.js";
+import {
+  createResponse,
+  serverErrorResponse,
+} from "../../helpers/responseHandler.js";
 import Order from "../../models/order.model.js";
 
 import { calculateDateRange } from "../../utils/CalculateDateRange.js";
 
-const generateSalesReport=async(req,res)=>{
-    console.log('inside generate Sales Report')
-    const { dateRange, startDate, endDate } = req.body;
-    console.log(req.body)
-    try {
-        if(dateRange==='full-report'){
-            const salesReport = await Order.find({}).populate('user')
-            return     createResponse(res,200,true,"Sales Generatored Successfully",salesReport)
-        }
-        const { start, end } = calculateDateRange(dateRange, startDate, endDate);
-
-        const salesReport = await Order.find({
-            orderDate: { $gte: start, $lte: end },
-          }).populate('user')
-
-          console.log("salesReport",salesReport)
-          createResponse(res,200,true,"Sales Generatored Successfully",salesReport)
-    } catch (error) {
-       serverErrorResponse(res)
+const generateSalesReport = async (req, res) => {
+  console.log("inside generate Sales Report");
+  const { dateRange, startDate, endDate } = req.body;
+  console.log(req.body);
+  try {
+    if (dateRange === "full-report") {
+      const salesReport = await Order.find({
+        status: { $in: ["Confirmed", "Delivered", "Shipped"] },
+      }).populate("user");
+      return createResponse(
+        res,
+        200,
+        true,
+        "Sales Generatored Successfully",
+        salesReport
+      );
     }
-}
+    const { start, end } = calculateDateRange(dateRange, startDate, endDate);
 
+    const salesReport = await Order.find({
+      $and: [
+        { status: { $in: ["Confirmed", "Delivered", "Shipped"] } },
+        { orderDate: { $gte: start, $lte: end } },
+      ],
+    }).populate("user");
 
+    console.log("salesReport", salesReport);
+    createResponse(
+      res,
+      200,
+      true,
+      "Sales Generatored Successfully",
+      salesReport
+    );
+  } catch (error) {
+    serverErrorResponse(res);
+  }
+};
 
-
-
-export{
-    generateSalesReport
-}
-
-
-
-
-
+export { generateSalesReport };
